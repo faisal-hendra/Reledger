b57eb1acf65f2e97f31d3868e67201c4458bc528
Faisal Hendra <faisalhw06@gmail.com>
2026-06-05 20:48:24 +0700

    Add auto-update

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,27 +1,39 @@
-import { app, BrowserWindow } from 'electron';
-import { createWindow } from './window';
-import { setupApp } from './lib/setup';
-import AppDatabase from './services/database';
-import { registerAllHandlers } from './ipc';
+import { app, BrowserWindow } from "electron";
+import { createWindow } from "./window";
+import { setupApp } from "./lib/setup";
+import { registerAllHandlers } from "./ipc";
+import AppDatabase from "./services/database";
+import { updateElectronApp, UpdateSourceType } from "update-electron-app";
 
 let db: AppDatabase;
 
+const GITHUB_REPO_URL = "faisal-hendra/Reledger";
+
 setupApp();
+
+updateElectronApp({
+  updateSource: {
+    type: UpdateSourceType.ElectronPublicUpdateService,
+    repo: GITHUB_REPO_URL,
+  },
+  updateInterval: "1 hour",
+  logger: require("electron-log"),
+});
 
 app.whenReady().then(() => {
   db = new AppDatabase();
   registerAllHandlers(db);
   createWindow();
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
